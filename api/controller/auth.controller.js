00976a0faeac4f9ba6b156d089afa28a29292802
Prince <prinsvaghasiya.it21@scet.ac.in>
2024-08-22 14:48:58 +0530

    create sigin api route backend

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 const User = require("../models/user.model");
 const bcryptjs = require("bcryptjs");
 const errorHandler = require("../utils/error");
+const jwt = require("jsonwebtoken");
 
 const signup = async (req, res, next) => {
   try {
@@ -20,7 +21,33 @@ const signup = async (req, res, next) => {
     next(errorHandler(500, `Error in the Signup backend: ${error.message}`));
   }
 };
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const isValidPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!isValidPassword) {
+      return next(errorHandler(401, "wrong cradentials"));
+    }
+    const token = jwt.sign({ userId: validUser._id }, process.env.SECRET_KEY);
+
+    const userObject = validUser.toObject();
+    const { password: hashedPassword, ...rest } = userObject;
+
+    const expiryDate = new Date(Date.now() + 36000000);
+    res
+      .cookie("token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(errorHandler(500, `Error in the Signiin backend: ${error.message}`));
+  }
+};
 
 module.exports = {
   signup,
+  signin,
 };
