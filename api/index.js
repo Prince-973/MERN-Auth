6f9d440a98718df9c6895e8686f31123129a97d7
Prince <prinsvaghasiya.it21@scet.ac.in>
2024-08-22 10:52:30 +0530

    add mongodb connection

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("connected to mongoDB");
+  })
+  .catch((err) => {
+    console.log("MongoDB ERROR:", err);
+  });
 
 app.listen(3000, () => {
   console.log("server listing on port 3000");
