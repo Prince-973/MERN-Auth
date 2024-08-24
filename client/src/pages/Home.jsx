a1166a8da85be40b7f1734d3e77ba52fd334d85f
Prince <prinsvaghasiya.it21@scet.ac.in>
2024-08-24 13:56:16 +0530

    complete home page and about us page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        Welcome to my Auth App!
+      </h1>
+      <p className="mb-4 text-slate-700">
+        The authentication boilerplate code provides a foundational setup for
+        integrating secure user authentication into web applications. It
+        includes essential features such as user registration, login, logout,
+        and password recovery. The boilerplate is designed to be flexible and
+        easy to integrate, supporting various authentication methods, including
+        JWT (JSON Web Tokens), OAuth, and traditional session-based
+        authentication. It follows best practices for security, such as hashing
+        passwords with bcrypt and protecting routes with middleware. This
+        boilerplate serves as a reliable starting point for developers looking
+        to implement a robust and scalable authentication system quickly.
+      </p>
+    </div>
+  );
 }
 
 export default Home;
